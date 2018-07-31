1135ee12be22972ef390483f3b83c32947b7b5ea
Nicole Jesudason <nicole.jesudason@gmail.com>
2018-08-01 08:05:20 +1000

    background moving gradient

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+  }
+
   render() {
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="introduction">
+          <h3>I'm Nicole Jesudason</h3>
+          <p>I am the world's greatest web developer</p>
+        </div>
       </div>
     );
   }
